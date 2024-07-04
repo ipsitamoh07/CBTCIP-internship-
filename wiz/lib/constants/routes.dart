236b96ef49e62830b1cd1e6d9b5a46251ebfd488
ipsitamoh07 <138458933+ipsitamoh07@users.noreply.github.com>
2024-07-04 23:22:04 +0530

    Delete wiz directory

--- wiz/lib/constants/routes.dart
+++
@@ -1,6 +0,0 @@
-const  loginRoute ='./login/';
-const  registerRoute ='./register/';
-const  verifyRoute ='./verify/';
-const  homeRoute ='./home/';
-const  mainViewRoute ='./mainview/';
-const newNoteRoute ='./notes/new-note/';
